34f0f0d461ded8604ab036c03f9f8ca857aad23b
Duc Huy Nguyen <stdcnguy>
2022-06-09 20:32:03 +0200

    GUI improvement

--- PawnRevolt.py
+++ PawnRevolt.py
@@ -1,5 +1,6 @@
-from easyAI import TwoPlayerGame
+import time
 
+from easyAI import TwoPlayerGame
 
 class PawnRevolt(TwoPlayerGame):
     def __init__(self, players, boardSize=(5, 3)):
@@ -7,7 +8,6 @@ class PawnRevolt(TwoPlayerGame):
         self.current_player = 1
         self.boardSize = boardSize
         self.board = self.initBoard()
-
 
     def possible_moves(self):
         return self.getAllPossibleMoves(self.board, color=str(self.current_player))
@@ -50,11 +50,12 @@ class PawnRevolt(TwoPlayerGame):
         return False, '0'
 
     def getAllPossibleMoves(self, board, color):
-        return (
-            self.getAllPossibleMovesFor1(board)
-            if color == '1'
-            else self.getAllPossibleMovesFor2(board)
-        )
+        possibleMoves = (self.getAllPossibleMovesFor1(board)
+                         if color == '1'
+                         else self.getAllPossibleMovesFor2(board))
+        return list(map(lambda move: "ABCDEFGHIJ"[move[0]] + str(move[1]) + " " + "ABCDEFGHIJ"[move[2]] + str(move[3]),
+                        possibleMoves))
+
 
     def isInBound(self, board, i, j):
         return 0 <= i < len(board) and 0 <= j < len(board[0])
@@ -101,7 +102,10 @@ class PawnRevolt(TwoPlayerGame):
         return possibleMoves
 
     def move(self, board, move):
-        originI, originJ, destinationI, destinationJ = move
+        move = tuple(map(lambda s: ("ABCDEFGHIJ".index(s[0]), int(s[1:])), move.split(" ")))
+        origin, destination = move
+        originI, originJ = origin
+        destinationI, destinationJ = destination
         originPiece = board[originI][originJ]
         board[originI][originJ] = '.'
         board[destinationI][destinationJ] = originPiece
@@ -115,15 +119,20 @@ class PawnRevolt(TwoPlayerGame):
 
         print("")
         for i in range(len(board)):
-            print(i, end="")
+            print("ABCDEFGHI"[i], end="")
             for j in range(len(board[i])):
                 print(f' {board[i][j]}', end="")
             print("")
         print()
 
+
 if __name__ == "__main__":
     from easyAI import AI_Player, Human_Player, Negamax
-    ai = Negamax(6)
-    game = PawnRevolt([Human_Player(), AI_Player(ai)])
+    start = time.time()
+    ai = Negamax(8)
+    # game = PawnRevolt([Human_Player(), AI_Player(ai)])
+    game = PawnRevolt([AI_Player(ai), AI_Player(ai)])
     game.play()
+    end = time.time()
+    print("Time:", end - start)
     print("player %d wins after %d turns " % (game.opponent_index, game.nmove))
--- State.py
+++
@@ -1,6 +0,0 @@
-class State:
-    def __init__(self, board, isEnd = False, currentPlayerTurn = 'Blue'):
-        self.board = board
-        self.isEnd = isEnd
-        self.currentPlayerTurn = currentPlayerTurn
-
